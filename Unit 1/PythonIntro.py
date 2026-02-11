4aaef50be34f37329ef0bff635b9871fd3d8e49c
Ian <38480036+Iankimble@users.noreply.github.com>
2026-02-11 13:45:03 +0000

    completed intro work

---
+++ Unit 1/PythonIntro.py
@@ -0,0 +1,39 @@
+bankAccount = 1100.23
+groceryTotal = 123.94
+numberOfAssignments = 8
+numberOfPizzas = 10
+
+# Data types = the most fundamental component of programming
+
+# Variable = a container for data (*data = data types)
+
+# Integer = a whole number 
+
+# Float = a decimal number
+
+# String = anything wrapped in quotations is a string data type. 
+# data "strung" together by quotations. 
+
+"hello"
+"about my grade"
+
+# Boolean = represent true of false values.
+
+
+x = 10
+y = 20
+z = 2.343
+print("hey are you going to the study hall later? ")
+print(x - y) 
+
+print(True)
+
+print(y + z)
+
+
+
+
+
+
+
+
