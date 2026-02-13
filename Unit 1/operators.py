95cbcb1a8195bd72b034ce132ae0cbe3a1ed75d6
Ian <38480036+Iankimble@users.noreply.github.com>
2026-02-13 13:04:08 +0000

    bhm report

---
+++ Unit 1/operators.py
@@ -0,0 +1,26 @@
+# Operator - a way for data types to 
+# interact and do things.
+
+# Assignment Operator Family
+# " assignment operators assign values to variables."
+# " values = data types."
+# " we represent the assignment operator "
+# " with a SINGLE equal sign"
+
+name = 'Ian'
+score = 90
+inSchool = True
+
+# Comparison Operator Family
+# comparison operators are used to compare values
+# values = data = data types
+# when you run code using comparisons it will return true or false
+
+print(10 < 2) 
+print(10 == 10) # 2 EQUAL SIGNS MEAN SAME AS
+print(10 != 2) # NOT THE SAME
+print(9 >= 1) # greater/less than OR EQUAL 
+
+# Arithmetic Operator Family
+
+# Logical Operator Family
