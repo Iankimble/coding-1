a66a664567f28537c0aaab2a01ef4be18ab5db7c
Ian <38480036+Iankimble@users.noreply.github.com>
2026-02-27 13:22:29 +0000

    built in function review

---
+++ Unit 1/builtinFunctionReview.py
@@ -0,0 +1,19 @@
+#1. Convert the following into an interger and print the result
+age = "16"
+
+#2. Convert the following into an string and print the result
+score = 85
+
+#3. Convert the following into an float and print the result
+price = 19.99
+
+#4. Convert the following variables into integers and then multiply them together
+# and print the result. 
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+#5. Fix the following code block
+print('It will cost' + 120.00 + ' dollars to to get everyone lunch.' )
+
+#6. Fix the following code block - result should be 104
+print("100" + 4)
