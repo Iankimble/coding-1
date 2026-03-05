98ed245bb07cdb2db08a7b4db447d3d8b8fc0232
Ian <38480036+Iankimble@users.noreply.github.com>
2026-03-05 12:19:39 +0000

    class work 3/5

--- Unit 1/builtinFunctionReview.py
+++ Unit 1/builtinFunctionReview.py
@@ -1,19 +1,36 @@
-#1. Convert the following into an interger and print the result
+#1. Convert the following into an interger and print the result 
 age = "16"
+
+print(type("16")) # --> this will give ... str 
+# type = shows you what data type that thing is
+
+print(int(age)) # int() --> anything passed in here will transform into a integer
 
 #2. Convert the following into an string and print the result
 score = 85
+print(str(score)) # --> anything passed in here will transform into a string
 
 #3. Convert the following into an float and print the result
 price = 19.99
 
+# Already a float - does not need to be convert
+
 #4. Convert the following variables into integers and then multiply them together
 # and print the result. 
-num1 = input("Enter a number: ")
+num1 = input("Enter a number: ") # anything passed in here automatically becomes
+# a string
 num2 = input("Enter another number: ")
 
+print('this is the first number we typed ' + num1)
+print('here is the second number we typed ' + num2)
+
+print(int(num1) * int(num2)) # int  * int
+
 #5. Fix the following code block
-print('It will cost' + 120.00 + ' dollars to to get everyone lunch.' )
+
+print('It will cost ' + str(120.00) + ' dollars to to get everyone lunch.' )
+
 
 #6. Fix the following code block - result should be 104
-print("100" + 4)
+print(int("100") + 4)
+
---
+++ Unit 2/userDefinedFunctions.py
@@ -0,0 +1,17 @@
+# User defined functions - code instructions that YOU write/ create.
+
+# How to write a function
+# part 1- function definition
+# () --> round brackets/ parameters
+def welcome_message():
+    print('hello good morning.') 
+
+# part 2 - function invocation/ call
+# welcome_message()
+
+def calculate():    
+    num1 = int(input("please type in a number: "))
+    num1 += 10
+    print(num1)
+
+calculate()
