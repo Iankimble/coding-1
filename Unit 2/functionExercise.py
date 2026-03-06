6fe213399499f49fa37b0660ff2b4de32ace5151
Ian <38480036+Iankimble@users.noreply.github.com>
2026-03-06 13:49:17 +0000

    class activity

---
+++ Unit 2/functionExercise.py
@@ -0,0 +1,25 @@
+#1. What keyword is used to define a function in Python?
+# Please write your response as a string.
+
+#2. What is the the difference between a parameter and an
+# argument?
+# Please write your response as a string.
+
+#3. Create a function that will take two parameters and then multiply
+# the two parameters together and print the result
+
+# --> create a function
+# --> needs 2 parameters
+# --> multiply the 2 parameters
+#--> print(result)
+
+def calculate(number1, number2):
+    result = number1* number2
+    print(result)
+    # print(number1 *number2)
+   
+calculate(10,9)
+
+#4. Create a function that will ask the user to input their name 
+# and a number, on seperate lines. When the function is ran, 
+# print the name and the number 
--- Unit 2/userDefinedFunctions.py
+++ Unit 2/userDefinedFunctions.py
@@ -7,11 +7,43 @@ def welcome_message():
     print('hello good morning.') 
 
 # part 2 - function invocation/ call
-# welcome_message()
+welcome_message()
 
-def calculate():    
-    num1 = int(input("please type in a number: "))
+def calculate(num1):    
+    # num1 = int(input("please type in a number: "))
     num1 += 10
     print(num1)
 
-calculate()
+calculate(10)
+
+# practice
+def foodOrder():
+    print('hamburger, chicken sandwich, salad')
+    order = input("what would you like to order?")
+    print(order)
+
+foodOrder()
+
+# Parameters and Arguments
+
+# Parameters and arguments are data that can be passed inside
+# of a function to follow instructions. 
+
+# num1 and num2 are parameters. these are placeholders for data.
+#P= parameter == P= placeholder
+
+# this placeholder data is not REAL. it is intended to be a 
+# reserve for actual incoming data.
+# this is used in the function definition. 
+
+def findAvg(num1, num2):
+    print(num1 +num2)
+
+# Arguments are REAL data in the function call
+# There must be the same number of arguments as there are parameters
+# otherwise you will get an error. 
+# Argument == real data --> in court to make a argument you 
+# need facts
+
+findAvg(10,9)
+
