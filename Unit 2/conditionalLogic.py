f6a3233b65654f2ea504d815aa432ea701fbeca1
Ian <38480036+Iankimble@users.noreply.github.com>
2026-03-18 12:21:55 +0000

    work form today

---
+++ Unit 2/conditionalLogic.py
@@ -0,0 +1,17 @@
+# Conditional Logic - A special class of function that
+# lets use run specific instructions based on specific 
+# data
+
+# we use the if and else keywords to run different 
+# instructions
+
+# Conditional syntax - how to write a conditional block
+absences = 0
+
+if absences == 0:
+    print("you have a dress down day.") 
+    # if is the condition we are looking for
+else:
+    print("you must come in uniform.")
+    # else is our default/ exit. what we want to happen
+    # if we CANT find the data we are looking for. 
