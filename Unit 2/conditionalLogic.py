f0e1f0fae2b4bfaf773937eb9e9f71bc4dc12025
Ian <38480036+Iankimble@users.noreply.github.com>
2026-03-19 11:50:09 +0000

    activity march 19

--- Unit 2/conditionalLogic.py
+++ Unit 2/conditionalLogic.py
@@ -6,6 +6,7 @@
 # instructions
 
 # Conditional syntax - how to write a conditional block
+
 absences = 0
 
 if absences == 0:
@@ -15,3 +16,23 @@ else:
     print("you must come in uniform.")
     # else is our default/ exit. what we want to happen
     # if we CANT find the data we are looking for. 
+
+creditsToPass = 30
+
+currentCredits = int(input("how many credits do you have? "))
+
+if currentCredits < creditsToPass:
+    print('sorry you do not have enough credits')
+else:
+    print("congrats you are graduating this year")
+
+schoolYear = input("What year of High School are you in? ")
+
+if schoolYear == 'Freshman':
+    print('you will be taking intro to high school')
+elif schoolYear =='Sophmore':
+    print('you have to take state testing')
+elif schoolYear == 'Junior':
+    print('you need an internship')
+else:
+    print('sorry this info does not apply to you.')
